db195f62ec05def32ab11563cafdee3d59a65fc7
VenomRishi <rishikesh.mhatre@instarem.com>
2021-01-20 15:43:58 +0530

    added: controller service and validator

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,47 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { TasksService } from './tasks.service';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { GetTasksFilterDTO } from './dto/get-tasks.filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+
+
+@Controller('/tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) { }
+
+  @Get()
+  getTasks(
+    @Query(ValidationPipe) filterDTO: GetTasksFilterDTO
+  ): Task[] {
+    if(Object.keys(filterDTO).length) {
+      return this.tasksService.getTasksWithFilters(filterDTO);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+    
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(
+    @Body() createTaskDTO: CreateTaskDTO
+  ): Task {
+    return this.tasksService.createTask(createTaskDTO);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): void {
+    return this.tasksService.deleteTaskById(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(@Param('id') id: string, @Body('status', TaskStatusValidationPipe) taskStatus: TaskStatus): Task {
+    return this.tasksService.updateTaskStatus(id, taskStatus);
+  }
+}
